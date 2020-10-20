89951519d56c2e5633dc9ca83c9f9aed993edf87
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-20 15:44:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
   if input % 3 == 0 && input % 5 == 0
-    "Fizzbuzz"
+    "FizzBuzz"
   
   elsif input % 3 == 0 
     "Fizz"
